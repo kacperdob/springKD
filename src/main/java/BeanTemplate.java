d25d28d07f68a17b6247047470c42c344a9e492b
Kacper Dobrowolski <kacper.dob@gmail.com>
2019-07-21 13:22:06 +0200

    second commit

---
+++ src/main/java/BeanTemplate.java
@@ -0,0 +1,38 @@
+public class BeamTemplate {
+    private String message1;
+    private String message2;
+    private String message3;
+
+    public BeamTemplate(){
+    }
+
+    public BeamTemplate(String message1, String message2, String message3) {
+        this.message1 = message1;
+        this.message2 = message2;
+        this.message3 = message3;
+    }
+
+    public String getMessage1() {
+        return message1;
+    }
+
+    public void setMessage1(String message1) {
+        this.message1 = message1;
+    }
+
+    public String getMessage2() {
+        return message2;
+    }
+
+    public void setMessage2(String message2) {
+        this.message2 = message2;
+    }
+
+    public String getMessage3() {
+        return message3;
+    }
+
+    public void setMessage3(String message3) {
+        this.message3 = message3;
+    }
+}
---
+++ src/main/java/Computer.java
@@ -0,0 +1,11 @@
+public class Computer {
+    private HardDisc hardDisc;
+
+    public HardDisc getHardDisc() {
+        return hardDisc;
+    }
+    public void setHardDisc(HardDisc hardDisc){
+
+    }
+    
+}
